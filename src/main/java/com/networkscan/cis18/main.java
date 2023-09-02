6aaa0072bd7df7cf5746e42a9df7d32475ee9469
AidanHiggs <aashburnhiggs112@mycr.redwoods.edu>
2023-09-01 17:23:57 -0700

    created a basic port scan function, it is working but needs to be refined.

--- src/main/java/com/networkscan/cis18/main.java
+++ src/main/java/com/networkscan/cis18/main.java
@@ -2,7 +2,8 @@ package com.networkscan.cis18;
 
 public class main {
     public static void main(String[] args) {
-        JavaPingExampleProgram ping = new JavaPingExampleProgram();
-        
+        portScanner scanner = new portScanner();
+        scanner.getInputs();
+        scanner.closeInput();
     }
 }
