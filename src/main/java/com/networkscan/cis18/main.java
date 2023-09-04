a1b126a94da51fdc92e6651f0d2e7aa8853317ad
AidanHiggs <aashburnhiggs112@mycr.redwoods.edu>
2023-09-04 13:51:07 -0700

    modified:   src/main/java/com/networkscan/cis18/main.java

--- src/main/java/com/networkscan/cis18/main.java
+++ src/main/java/com/networkscan/cis18/main.java
@@ -1,9 +1,16 @@
 package com.networkscan.cis18;
+import javax.swing.SwingUtilities;
+
+
+import javax.swing.SwingUtilities;
 
 public class main {
     public static void main(String[] args) {
-        portScanner scanner = new portScanner();
-        scanner.getInputs();
-        scanner.closeInput();
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                NetworkScannerGUI scanner = new NetworkScannerGUI();
+                scanner.setVisible(true);
+            }
+        });
     }
 }
