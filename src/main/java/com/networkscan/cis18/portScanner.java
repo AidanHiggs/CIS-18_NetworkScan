05f238a477444c6100286e24e1ca40afbdefd702
AidanHiggs <aashburnhiggs112@mycr.redwoods.edu>
2023-09-05 11:42:22 -0700

    addedd comments

--- src/main/java/com/networkscan/cis18/portScanner.java
+++ src/main/java/com/networkscan/cis18/portScanner.java
@@ -45,30 +45,30 @@ public class portScanner {
         return endPort;
     }
 
-    /**
-     * Loads port services from a file and populates the portServicesMap.
-     *
-     * @param  filename  the name of the file containing the port services
-     */
-    public static void loadPortServices(String filename) {
-        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] parts = line.split(" ");
-                if (parts.length >= 3) {
-                    String key = parts[0] + "-" + parts[1]; // e.g., "22-tcp"
-                    String service = "";
-                    for (int i = 2; i < parts.length; i++) {
-                        service += parts[i] + " ";
-                    }
-                    service = service.trim(); 
-                    portServicesMap.put(key, service);
+/**
+ * Loads port services from a file and populates the portServicesMap.
+ *
+ * @param  filename  the name of the file containing the port services
+ */
+public static void loadPortServices(String filename) {
+    try (BufferedReader br = new BufferedReader(new FileReader(filename))) { // Open the file for reading
+        String line;
+        while ((line = br.readLine()) != null) { // Read each line from the file
+            String[] parts = line.split(" "); // Split the line by spaces
+            if (parts.length >= 3) { // Check if there are at least 3 parts
+                String key = parts[0] + "-" + parts[1]; // Create a key by concatenating the first two parts
+                String service = ""; // Initialize an empty string for the service
+                for (int i = 2; i < parts.length; i++) { // Iterate over the remaining parts
+                    service += parts[i] + " "; // Concatenate each part with a space
                 }
+                service = service.trim(); // Remove leading and trailing spaces from the service
+                portServicesMap.put(key, service); // Add the key-value pair to the map
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+    } catch (IOException e) { // Catch any IOException that occurs during file reading
+        e.printStackTrace(); // Print the stack trace for debugging
     }
+}
     
 
     public static String getService(int port, String protocol) {
@@ -77,44 +77,62 @@ public class portScanner {
 
 
     // Method to scan the IP address for open ports
-    public static void scanIp(int startPort, int endPort, String ipAddr, JTextArea resultArea) {
-        SwingWorker<String, Void> worker = new SwingWorker<String, Void>() {
-            @Override
-            protected String doInBackground() throws Exception {
-                StringBuilder sb = new StringBuilder();
-                for (int port = startPort; port <= endPort; port++) {
-                    try {
-                        Socket socket = new Socket();
-                        socket.connect(new InetSocketAddress(ipAddr, port), 5000);
-                        String protocol = "tcp"; // You can change this to "udp" if needed
-                        String key = port + "-" + protocol;
-                        String service = getService(port, protocol);
-                        if (service.equals("Unknown")) {
-                            System.out.println("Service not found for key: " + key);
-                        }
-                        sb.append("Port " + port + " is open - " + service + "\n");
-                        socket.close();
-                    } catch (IOException e) {
-                        // Ignore exceptions for closed ports
-                    }
-                }
-                return sb.toString();
-            }
-    
-            @Override
-            protected void done() {
+    // Method to scan the IP address for open ports
+public static void scanIp(int startPort, int endPort, String ipAddr, JTextArea resultArea) {
+    // Create a SwingWorker to perform the scanning task asynchronously
+    SwingWorker<String, Void> worker = new SwingWorker<String, Void>() {
+        // Override the doInBackground method to perform the scanning task
+        @Override
+        protected String doInBackground() throws Exception {
+            // Create a StringBuilder to store the scan results
+            StringBuilder sb = new StringBuilder();
+            // Iterate through the range of ports to scan
+            for (int port = startPort; port <= endPort; port++) {
                 try {
-                    String scanResult = get();
-                    SwingUtilities.invokeLater(() -> resultArea.append(scanResult));
-                } catch (Exception e) {
-                    e.printStackTrace();
+                    // Create a socket and connect to the IP address and port
+                    Socket socket = new Socket();
+                    socket.connect(new InetSocketAddress(ipAddr, port), 5000);
+                    // Specify the protocol (tcp or udp)
+                    String protocol = "tcp";
+                    // Create a key to identify the service based on the port and protocol
+                    String key = port + "-" + protocol;
+                    // Get the service associated with the port and protocol
+                    String service = getService(port, protocol);
+                    // If the service is unknown, print a message
+                    if (service.equals("Unknown")) {
+                        System.out.println("Service not found for key: " + key);
+                    }
+                    // Append the open port and service to the scan results
+                    sb.append("Port " + port + " is open - " + service + "\n");
+                    // Close the socket
+                    socket.close();
+                } catch (IOException e) {
+                    // Ignore exceptions for closed ports
                 }
             }
-    
-        };
-    
-        worker.execute();
-    }
+            // Return the scan results
+            return sb.toString();
+        }
+
+        // Override the done method to update the UI with the scan results
+        @Override
+        protected void done() {
+            try {
+                // Get the scan results from the doInBackground method
+                String scanResult = get();
+                // Update the UI with the scan results using the Event Dispatch Thread
+                SwingUtilities.invokeLater(() -> resultArea.append(scanResult));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    };
+
+    // Execute the SwingWorker to start the scanning task
+    worker.execute();
+}
+
 
     // Method to resolve the hostname to an IP address
    public static String resolveHostname(String host, JTextArea resultArea) {
