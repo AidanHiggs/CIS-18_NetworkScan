e11fef92b6509418b8fd7ea642bf01f4bc273e94
AidanHiggs <aashburnhiggs112@mycr.redwoods.edu>
2023-10-11 16:17:20 -0700

    modified:   src/main/java/com/networkscan/cis18/Main.java 	modified:   src/main/java/com/networkscan/cis18/hostFactory.java 	modified:   src/main/java/com/networkscan/cis18/hostUpdate.java 	deleted:    src/main/java/com/networkscan/cis18/impliments.java 	modified:   src/main/java/com/networkscan/cis18/osDetectionDecorator.java

--- src/main/java/com/networkscan/cis18/hostFactory.java
+++ src/main/java/com/networkscan/cis18/hostFactory.java
@@ -2,6 +2,7 @@ package com.networkscan.cis18;
 
 public class hostFactory {
 <<<<<<< HEAD
+<<<<<<< HEAD
     private static int hostCount = 0;
 
     public static hostImpl createHost(String ipAddress) {
@@ -12,6 +13,18 @@ public class hostFactory {
     public static host createHost(String ipAddress) {
         host host = new hostImpl();
 >>>>>>> e362fa1 (	modified:   src/main/java/com/networkscan/cis18/Main.java)
+=======
+    public static volatile int id = 0;
+    public static host createHost(String ipAddress) {
+        host host = new hostImpl();
+=======
+    private static int hostCount = 0;
+
+    public static hostImpl createHost(String ipAddress) {
+        hostCount++;
+        hostImpl host = new hostImpl();
+>>>>>>> 5fb6d38 (	modified:   src/main/java/com/networkscan/cis18/Main.java)
+>>>>>>> 45d4d14 (	modified:   src/main/java/com/networkscan/cis18/Main.java)
         host.setIpAddress(ipAddress);
         host.setId(id++);
         return host;
