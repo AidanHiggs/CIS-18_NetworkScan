83b87f02ca31dc2a5947903b18131a9bd0ca1ca2
AidanHiggs <aashburnhiggs112@mycr.redwoods.edu>
2023-12-06 13:33:47 -0800

    modified:   src/main/java/com/networkscan/cis18/NetworkScannerController.java 	modified:   src/main/java/com/networkscan/cis18/hostDisco.java 	modified:   src/main/java/com/networkscan/cis18/setNet.java

--- src/main/java/com/networkscan/cis18/NetworkScannerController.java
+++ src/main/java/com/networkscan/cis18/NetworkScannerController.java
@@ -2,21 +2,27 @@ package com.networkscan.cis18;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.InetAddress;
+import java.util.ArrayList;
 import java.util.List;
+import javax.swing.SwingWorker;
+
+import org.pcap4j.core.PcapHandle;
 
 public class NetworkScannerController {
     private NetworkScannerModel model;
     private NetworkScannerView view;
-    private portScanner portScanner; // Make sure this is initialized
-    private PingDecorator pingDecorator; // Make sure this is initialized
+    private portScanner portScanner;
+    private PingDecorator pingDecorator;
     private hostDisco hostDisco;
 
     public NetworkScannerController(NetworkScannerModel model, NetworkScannerView view,
                                     portScanner portScanner, PingDecorator pingDecorator, hostDisco hostDisco) {
         this.model = model;
         this.view = view;
-        this.portScanner = portScanner; // Ensure portScanner is provided here
-        this.pingDecorator = pingDecorator; // Ensure pingDecorator is provided here
+        this.portScanner = portScanner;
+        this.pingDecorator = pingDecorator;
+        this.hostDisco = hostDisco;
         initView();
     }
 
@@ -50,12 +56,50 @@ public class NetworkScannerController {
     }
 
     private void performHostDiscovery() {
-        try {
-            List<String> discoveredHosts = hostDisco.tcpSweep(model.getSubnetMask());
-            view.getResultArea().setText(String.join("\n", discoveredHosts));
-        } catch (Exception ex) {
-            view.getResultArea().setText("Error in host discovery: " + ex.getMessage());
-        }
+        SwingWorker<List<String>, Integer> worker = new SwingWorker<List<String>, Integer>() {
+            @Override
+            protected List<String> doInBackground() throws Exception {
+                hostDisco.setNetworkSettings();
+                int totalHosts = hostDisco.calculateDevices();
+                PcapHandle networkInt = (PcapHandle) setNet.getNetworkHandle(); // Get PcapHandle from networkSettings
+
+                List<InetAddress> addresses = hostDisco.generateHostAddresses(networkInt, totalHosts);
+                List<String> discoveredHosts = new ArrayList<>();
+                int count = 0;
+
+                for (InetAddress addr : addresses) {
+                    if (isCancelled()) {
+                        break;
+                    }
+                    if (hostDisco.sendTcpPacketAndCheckResponse(addr, networkInt)) {
+                        discoveredHosts.add(addr.getHostAddress());
+                        publish(++count * 100 / addresses.size());
+                    }
+                }
+
+                networkInt.close(); // Ensure the handle is closed after use
+                return discoveredHosts;
+            }
+
+            @Override
+            protected void process(List<Integer> chunks) {
+                int latestPercentage = chunks.get(chunks.size() - 1);
+                view.getResultArea().append("Scan completion: " + latestPercentage + "%\n");
+            }
+
+            @Override
+            protected void done() {
+                try {
+                    List<String> discoveredHosts = get();
+                    view.getResultArea().append("Scan completed. Discovered hosts:\n");
+                    view.getResultArea().append(String.join("\n", discoveredHosts));
+                } catch (Exception ex) {
+                    view.getResultArea().setText("Error in host discovery: " + ex.getMessage());
+                }
+            }
+        };
+
+        worker.execute();
     }
 
     private void performPortScan() {
