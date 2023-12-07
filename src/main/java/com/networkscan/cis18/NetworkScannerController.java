eb5bdd54dfe3f79c1f81214e7dd51bcd56e515a1
AidanHiggs <aashburnhiggs112@mycr.redwoods.edu>
2023-12-06 16:58:24 -0800

    modified:   src/main/java/com/networkscan/cis18/Main.java 	modified:   src/main/java/com/networkscan/cis18/NetworkScannerController.java 	modified:   src/main/java/com/networkscan/cis18/PingDecorator.java 	modified:   src/main/java/com/networkscan/cis18/hostDisco.java

--- src/main/java/com/networkscan/cis18/NetworkScannerController.java
+++ src/main/java/com/networkscan/cis18/NetworkScannerController.java
@@ -7,8 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.swing.SwingWorker;
 
-import org.pcap4j.core.PcapHandle;
-
 public class NetworkScannerController {
     private NetworkScannerModel model;
     private NetworkScannerView view;
@@ -59,40 +57,31 @@ public class NetworkScannerController {
         SwingWorker<List<String>, Integer> worker = new SwingWorker<List<String>, Integer>() {
             @Override
             protected List<String> doInBackground() throws Exception {
-                hostDisco.setNetworkSettings();
                 int totalHosts = hostDisco.calculateDevices();
-                PcapHandle networkInt = (PcapHandle) setNet.getNetworkHandle(); // Get PcapHandle from networkSettings
-
-                List<InetAddress> addresses = hostDisco.generateHostAddresses(networkInt, totalHosts);
                 List<String> discoveredHosts = new ArrayList<>();
-                int count = 0;
-
-                for (InetAddress addr : addresses) {
-                    if (isCancelled()) {
-                        break;
-                    }
-                    if (hostDisco.sendTcpPacketAndCheckResponse(addr, networkInt)) {
-                        discoveredHosts.add(addr.getHostAddress());
-                        publish(++count * 100 / addresses.size());
+                InetAddress[] addresses = hostDisco.generateHostAddresses(totalHosts);
+                
+                for (InetAddress address : addresses) {
+                    System.out.println("Scanning " + address.getHostAddress());
+                    pingDecorator.addPingResolution(address.getHostAddress());
+                    String[] results = pingDecorator.getPingResults();
+                    for (String result : results) {
+                        if (result != null && result.contains("Reply from")) {
+                            discoveredHosts.add(address.getHostAddress());
+                            break;
+                        } else {
+                            System.out.println("No reply from " + address.getHostAddress());
+                        }
                     }
                 }
-
-                networkInt.close(); // Ensure the handle is closed after use
                 return discoveredHosts;
-            }
-
-            @Override
-            protected void process(List<Integer> chunks) {
-                int latestPercentage = chunks.get(chunks.size() - 1);
-                view.getResultArea().append("Scan completion: " + latestPercentage + "%\n");
             }
 
             @Override
             protected void done() {
                 try {
                     List<String> discoveredHosts = get();
-                    view.getResultArea().append("Scan completed. Discovered hosts:\n");
-                    view.getResultArea().append(String.join("\n", discoveredHosts));
+                    view.getResultArea().setText(String.join("\n", discoveredHosts));
                 } catch (Exception ex) {
                     view.getResultArea().setText("Error in host discovery: " + ex.getMessage());
                 }
